24f2c3dd8b579d7af12e1f4ff3d71eada3d0894a
Adam Johnson <adam@pkqk.net>
2009-09-24 09:55:50 +0100

    Bump version number

--- reevoo_mark.php
+++ reevoo_mark.php
@@ -122,7 +122,7 @@
       curl_setopt($ch, CURLOPT_URL, $this->remote_url);
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt($ch, CURLOPT_TIMEOUT_MS, 2000);
-      curl_setopt($ch, CURLOPT_USERAGENT, "ReevooMark PHP Widget/1");
+      curl_setopt($ch, CURLOPT_USERAGENT, "ReevooMark PHP Widget/2");
       curl_setopt($ch, CURLOPT_REFERER, "http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT_MS, 2000);
       curl_setopt($ch, CURLOPT_HEADER, 1);

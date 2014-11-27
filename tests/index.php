d79b131bc8e680ae20979c4610c92232e3884794
David Sevcik <david.sevcik@gmail.com>
2014-11-27 17:37:04 +0000

    Include all test files

--- tests/index.php
+++ tests/index.php
@@ -2,7 +2,8 @@
   require_once('simpletest/autorun.php');
 
   $test = new TestSuite('All file tests');
-  $test->addFile('./reevoo_mark_test.php');
-  $test->addFile('./reevoo_mark_document_test.php');
+  $test->addFile(dirname(__FILE__) . '/reevoo_mark_test.php');
+  $test->addFile(dirname(__FILE__) . '/reevoo_mark_document_test.php');
+  $test->addFile(dirname(__FILE__) . '/reevoo_mark_http_client_test.php');
   exit ($test->run(new TextReporter()) ? 0 : 1);
 ?>
